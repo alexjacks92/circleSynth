006c6bfc4559858428fa08fdbc1755578eaf60c6
alexjacks92 <alexjacks@comcast.net>
2014-11-27 18:18:51 -0500

    Sound is being played, but duration isn't right.

--- main.lua
+++ main.lua
@@ -1,18 +1,23 @@
 function love.load()
     shapes = {}
+    sounds = {}
     radius = 10
     -- dp: delta position, dr: delta radius
     love.keyboard.setKeyRepeat(true)
     var = 10
     rate = 3
+    sine = love.audio.newSource("sine.wav", "static")
+    sine:setLooping(false)
 end
 
 function love.update(dt)
     for index,shape in pairs(shapes) do
-        if (shape:getRadius() < 250) then
+        if (shape:getRadius() < 550) then
             shape:setRadius(shape:getRadius() + rate)
          else
             shape:setRadius(50)
+            sounds[index]:seek(9, "seconds")
+            love.audio.play(sounds[index])
          end
     end
 end
@@ -27,7 +32,14 @@ end
 
 function love.mousepressed(x, y, button)
     newCircle = love.physics.newCircleShape(x, y, radius)
+    sine2 = sine:clone()
+    sine2:setPitch(math.random() + math.random())
+    print(sine2:getPitch())
+    sine2:setLooping(false)
     table.insert(shapes, newCircle)
+    table.insert(sounds, sine2)
+    print(#shapes)
+    print(#sounds)
 end
 
 
