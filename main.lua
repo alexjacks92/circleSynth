31f5a9527ad99331012e9933a396fca29107d690
alexjacks92 <alexjacks@comcast.net>
2014-11-28 02:49:05 -0500

    added comments.

--- main.lua
+++ main.lua
@@ -1,15 +1,18 @@
 function love.load()
     shapes = {}
     sounds = {}
-    radius = 10
-    -- dp: delta position, dr: delta radius
+    radius = 1 -- starting radius for circles
     love.keyboard.setKeyRepeat(true)
-    var = 10
-    rate = 3
-    sine = love.audio.newSource("sineShort.wav", "static")
+    rate = 1 -- grow rate for circle, should become an arg
+    sine = love.audio.newSource("sine.wav", "static")
     sine:setLooping(false)
+    chaos = false -- incoming arg: is it a circle, or crazy random poly shapes?
+    lineWidth = 1 -- incoming line width arg
+    color = random -- incoming color arg
 end
 
+-- grow circles, or "pop" them, IE take them back down to starting radius
+-- when a pop occurs, retrigger the sound (will make more sense when duration works)
 function love.update(dt)
     for index,shape in pairs(shapes) do
         if (shape:getRadius() < 550) then
@@ -23,24 +26,31 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setLineWidth(lineWidth)
     for index,shape in pairs(shapes) do
         love.graphics.setColor(math.random(0,255),math.random(0,255),math.random(0,255),255)
         shapeX, shapeY = shape:getPoint()
-        love.graphics.circle("line", shapeX, shapeY, shape:getRadius())
+        if chaos then
+            love.graphics.circle("line", shapeX, shapeY, shape:getRadius(), math.random(4, 20))
+        else
+            love.graphics.circle("line", shapeX, shapeY, shape:getRadius())
+        end
     end
 end
 
+-- make a new circle on mouse presses
 function love.mousepressed(x, y, button)
     newCircle = love.physics.newCircleShape(x, y, radius)
     sine2 = sine:clone()
-    
+    -- y coordinate dictates pitch
     local height = love.window.getHeight()
     local mod = math.floor(y / (height / 12))
     print(x)
     print(height)
     print(mod)
     sine2:setPitch(1 + (mod * (.08 + (1/300))))
-
+    
+    -- x coordinate dictates volume
     local width = love.window.getWidth()
     local modw = x / height
     print(y)
@@ -52,11 +62,10 @@ function love.mousepressed(x, y, button)
     sine2:setLooping(false)
     table.insert(shapes, newCircle)
     table.insert(sounds, sine2)
-    love.audio.play(sine2)
-    --print(#shapes)
-   -- print(#sounds)
+    love.audio.play(sine2) -- need to play the sound on the first click
 end
 
+-- I'm sure there will be more keys eventually, but for now, just quitting.
 function love.keypressed(key, isrepeat)
     if key == "escape" then
         love.event.quit()
